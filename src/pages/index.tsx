9a5baca6ecbacec53a4fea71759221022ac19f8a
Felipe <felipe.pereira.dev@gmail.com>
2021-02-23 14:18:53 -0300

    feat(component): experience bar content and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+
 export default function Home() {
   return (
-    <h1>Initial</h1>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   )
 }
