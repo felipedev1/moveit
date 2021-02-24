3cf38a0e3d2f11233e228cb8ade70610dcae600f
Felipe <felipe.pereira.dev@gmail.com>
2021-02-24 10:17:12 -0300

    feat(component): Countdown

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react'
+import styles from '../styles/components/Countdown.module.css'
+
+let countdownTimeout: NodeJS.Timeout
+
+export function Countdown() {
+  const [time, setTime] = useState(25 * 60)
+  const [isActive, setIsActive] = useState(false)
+  const [hasFinished, setHasFinished] = useState(false)
+
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+  const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
+
+  function startCountdown() {
+    setIsActive(true)
+  }
+
+  function resetCountdown() {
+    clearTimeout(countdownTimeout)
+    setIsActive(false)
+    setTime(25 * 60)
+  }
+
+  useEffect(() => {
+    if (isActive && time > 0) {
+      countdownTimeout = setTimeout(() => {
+        setTime(time - 1)
+      }, 1000)
+    } else if (isActive && time === 0) {
+      setHasFinished(true)
+      setIsActive(false)
+    }
+  }, [isActive, time])
+
+  return (
+    <div>
+      <div className={styles.countdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+
+      {hasFinished ? (
+        <button
+          disabled
+          className={styles.countdownButton}
+        >
+          Ciclo encerrado
+          <img src="icons/check_circle.svg" alt=""/>
+        </button>
+      ) : (
+          <>
+            {isActive ? (
+              <button
+                type="button"
+                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                onClick={resetCountdown}
+              >
+                Abandonar clico
+                <img src="icons/close.svg" alt=""/>
+              </button>
+            ) : (
+                <button
+                  type="button"
+                  className={styles.countdownButton}
+                  onClick={startCountdown}
+                >
+                  Iniciar um clico
+                  <img src="icons/play_arrow.svg" alt=""/>
+                </button>
+              )
+            }
+          </>
+        )}
+
+
+
+    </div>
+  )
+}
